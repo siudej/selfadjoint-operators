acc8e1f01a7124e6b87fc96c7120b7582d849fc3
Bartek Siudeja <siudej@gmail.com>
2016-03-15 21:44:34 -0700

    added rational computations

--- FourPointSpectrum.m
+++ FourPointSpectrum.m
@@ -2,10 +2,24 @@
 
 BeginPackage["FourPointSpectrum`"]
 
-image::usage = 
-"A few ways to use the function image[beta]:
+intervals::usage = 
+" Generate intervals for a given beta. "
 
-- Generate an image.
+image::usage = 
+" Plot intervals. "
+points::usage =
+" Plot the endpoints of the intervals. "
+
+numerical::usage = 
+" Numerical[True/False] force numerical computations, or try rationals. " 
+
+Begin["`Private`"]
+
+
+Print[
+" Generates the set of possible eigenvalues E1, E2 for self-adjoint operators with diagonal elements beta^k, 1-beta^k, assuming there are exactly 4 eigenvalues: 0, 1, E1, E2.
+
+- Generate an image:
 b = 95/100;
 im = image[b] // AbsoluteTiming;
 Export[\" image.png \", im[[2, 1]], ImageSize -> 2000]
@@ -19,12 +33,15 @@ Export[\"~/movie.avi\",animate[[2,All,1]],ImageSize->800]
 
 - Manipulate beta almost in real time:
 B=0.8
-Manipulate[image[b],{b,0.01,0.8}]"
+Manipulate[image[b],{b,0.01,0.8}]
 
-Begin["`Private`"]
+- Use points[beta] instead of image[beta] for the endpoints of the intervals.
 
+- Use intervals[beta] to get the intervals as a array.
 
-Print["Use image[beta] to generate the set of possible eigenvalues E1 and E2 for a class of self-adjoint operators (parametrized by beta), with the point spectrum equal to 0=E0<E1<E2<E3=1. "]
+- numerical[True/False] force numerical computations/try rationals
+"
+]
 
 (* Clean version of line endpoints with arbitrary beta *) 
 (* all multiplicities considered, based on epsilon *)
@@ -38,10 +55,11 @@ I2[a_, b_] := Ceiling[Log[1-a]/Log[b]]-1; (* nearest position to a, a>1/2 *)
 Cc[a_, b_] := C1[I1[a, b], b] + C2[I2[a, b], b];(* Sum C(a) *) 
 Mc[a_, b_] := I2[a, b] - I1[a, b]; (* Counting elements function m(a) *)
 
+numerical[bool_] := If[bool, NN[x_?NumberQ]:=N[x], NN[x_?NumberQ]:=x]
+numerical[True];
+SetAttributes[NN, Listable];
 
-(* Remove all N[] calls to get rational results. The performance remains very good. *)
-diag = Plot[x, {x,0,1}, PlotStyle->Dashed, PlotRange->{{0,1},{0,1}}, AspectRatio->1, Frame -> True]; 
-image[b_] := 
+intervals[b_] := 
 	Block[{t, endpoints, int, vI1, vI2, max, min, lend, cend, s, time, mid, vCc, vMc, i, k, M, a, tbl},
 		(*time = AbsoluteTime[];*)
 		(* endpoints of intervals on which Cc is constant *)
@@ -64,21 +82,21 @@ image[b_] :=
 					Sow[vCc[[k]]/(i+vMc[[k]])]; i+=1,
 					k-=1
 				]
-			  ]][[2,1]]//N;
+			  ]][[2,1]]//NN;
 		M = Length[max];
 		min = 1-max;
 		(*maxtime=AbsoluteTime[]- time;
 		time = AbsoluteTime[];*)
 		(* lengths of intervals *)
 		(* parallel is not always faster *)
-		tbl = If[M > 25, DistributeDefinitions[M, max, min, eps]; ParallelTable, Table];
+		tbl = If[M > 25, DistributeDefinitions[M, max, min, eps, NN]; ParallelTable, Table];
 		(* from the side *)
 		lend = tbl[
 			Which[
-			    GCD[N1,N2,m] > 1,
+			    GCD[N1, N2, m] > 1,
 			    Unevaluated@Sequence[],
-			    a = Min[max[[N1+N2-m]], N[m]/(N1+N2), (m-N2 min[[m]])/N1]; a>=eps,
-			    {{0., N[m]/N2}, {a, (m-N1 a)/N2}},
+			    a = Min[max[[N1+N2-m]], NN[m]/(N1+N2), (m-N2 min[[m]])/N1]; a>=eps,
+			    {{NN[0], NN[m]/N2}, {a, (m-N1 a)/N2}},
 			    True,
 			    Unevaluated@Sequence[]
 			],
@@ -87,10 +105,10 @@ image[b_] :=
 		(* from vertex *)
 		cend = tbl[
 			Which[
-			    GCD[N1,N2] > 1,
+			    GCD[N1, N2] > 1,
 			    Unevaluated@Sequence[],
-			    a = Min[max[[N1]], N[N2]/(N1+N2), N2 max[[N2]]/N1]; a>=eps,
-			    {{0.,1.},{a,1.-N1 a/N2}},
+			    a = Min[max[[N1]], NN[N2]/(N1+N2), N2 max[[N2]]/N1]; a>=eps,
+			    {{NN[0], NN[1]},{a, NN[1]-N1 a/N2}},
 			    True,
 			    Unevaluated@Sequence[]
 			],
@@ -100,10 +118,19 @@ image[b_] :=
  		(* rend is a reflection of lend about y=x *)
 		lend = Join[lend, RotateLeft[lend, {0,0,1}]];
 		(*lendtime=AbsoluteTime[]- time;*)
-		(* graph of line segements *)
-		s = Graphics[{AbsoluteThickness[1-b], Line[lend]}, PlotRange->{{0,1},{0,1}}, AspectRatio->1];
-		Labeled[Show[diag, s, ImageSize->400], N[b]]
+		lend
 ];
+
+diag = Plot[x, {x,0,1}, PlotStyle->Dashed, PlotRange->{{0,1},{0,1}}, AspectRatio->1, Frame -> True]; 
+image[b_] := Labeled[Show[diag, 
+			 Graphics[{AbsoluteThickness[1-b], Line[intervals[b]]}, 
+			 	  PlotRange->{{0,1},{0,1}}, AspectRatio->1], 
+			 ImageSize->400], N[b]]
+
+points[b_] := Labeled[Show[diag, 
+			 Graphics[{Point[intervals[b][[All,2]]]}, 
+			 	  PlotRange->{{0,1},{0,1}}, AspectRatio->1], 
+			 ImageSize->400], N[b]]
 
 End[]
 
